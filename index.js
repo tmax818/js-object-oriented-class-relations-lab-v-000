22c83d6ff6187ebd73d30957df99e3b286add659
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-06-01 17:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ class Trip {
   }
   
   passenger() {
-    console.log(store.passengers.filter(x => x.id === this.passengerId));
+    return store.passengers.filter(x => x.id === this.passengerId);
     console.log(this);
    }
 }
