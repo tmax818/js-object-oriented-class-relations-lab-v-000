acbc2caf428e37759a8bcf5fb5781f6504829cf9
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-06-01 17:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Driver {
   }
   
   trips() {
-    console.log(store.trips.filter(x => x.driverId === this.id));
+    console.log(store.trips.filter(x => {return x.driverId === this.id}));
   }
 }
 
